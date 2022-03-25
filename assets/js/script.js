cc6a7d05935276409dde60cf84e887808b0f8093
Jill Cunningham <jleighcunningham@gmail.com>
2022-03-24 21:28:22 -0400

    set conditional statements to assist with color block coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,28 @@
-var task = "";
-var taskBar = $(".taskBar");
-var saveButtonEl = $("saveBtn");
-
-var datetime = null,
-  date = null;
-
-var update = function () {
-  date = moment(new Date());
-  datetime.html(date.format("dddd, MMMM Do YYYY, h:mm:ss a"));
-  dueTime();
-};
-
-$(document).ready(function () {
-  datetime = $("#currentDay");
-  update();
-  setInterval(update, 1000);
-});
-
-saveButtonEl.click(function (event) {
-  event.preventDefault();
-  console.log($(this).siblings("textarea").val());
-  console.log($(this).attr("id"));
-  localStorage.setItem($(this).attr("id"), $(this).siblings("textarea").val());
-});
+function hourlyTasks() {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    
+    $(".saveBtn").on("click", function () {
+        var userTask = $(this).siblings(".task").val();
+        var timeslots = $(this).parent().attr("id");
+        localStorage.setItem(userTask, timeslots);
+        var presentHour = moment().hour();
+    
+        $(".hour").each(function () {
+            var chosenHour = parseInt($(this).attr("id").split("hour")[1]);
+    
+            if (chosenHour < presentHour) {
+                $(this).removeClass("present");
+                $(this).addClass("past");
+                $(this).removeClass("future");
+            }
+            else if (chosenHour === presentHour) {
+                $(this).addClass("present");
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
